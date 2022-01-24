0fcbe93d1b16a65cd9c239649d68648fe0350c45
jmjac <jakub@jackowiak.dev>
2022-01-24 00:19:44 -0500

    Fix crashes when blocks in sql don't match chain blocks

--- blockchain/blockchainState.go
+++ blockchain/blockchainState.go
@@ -14,7 +14,10 @@ type BlockchainState struct {
 	Identities       map[string]vrscClient.Identity `json:"-"`
 	LockedIdentities []identityWithBalance          `json:"-"`
 	Height           int
-	Stats            Stats `json:"-"`
+	BlockHash        string
+	Stats            Stats     `json:"-"`
+	StatsPerHour     []Summary `json:"-"`
+	StatsPerDay      []Summary `json:"-"`
 	store            *store.Store
 	verus            vrscClient.Verus
 }
@@ -47,38 +50,130 @@ func (b *BlockchainState) SetBlockchain(verus vrscClient.Verus) {
 	b.verus = verus
 }
 
-func LoadBlockchainState(verus vrscClient.Verus, store *store.Store) (BlockchainState, error) {
-	f, err := os.ReadFile("state2.json")
-	if err != nil {
-		return BlockchainState{}, nil
-	}
-	var b BlockchainState
-	err = json.Unmarshal(f, &b)
+func LoadBlockchainState(verus vrscClient.Verus, store *store.Store) BlockchainState {
+	b := BlockchainState{}
 	b.verus = verus
 	b.store = store
-	return b, err
+	b.Identities = make(map[string]vrscClient.Identity)
+	b.LockedIdentities = make([]identityWithBalance, 0)
+	//Check if the chain diverged
+	height, hash := b.store.GetTopBlock()
+	blockHash, _ := verus.GetBlockHash(height)
+	for blockHash != hash {
+		err := b.store.RemoveByHeight(int64(height))
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Block hash in store is different the on chain block hash. Removing block:", height)
+		height--
+		blockHash, _ = verus.GetBlockHash(height)
+	}
+	b.Height = height
+	return b
 }
 
-func (b *BlockchainState) scanBlock() error {
-	log.Println("Block: ", b.Height)
-	block, err := b.verus.GetBlockFromHeight(b.Height)
+//Save the block, transactions and transaction address mapping to databse
+func (b *BlockchainState) enterBlock(blockHeight int) (string, error) {
+	//TODO: Fix non-standard TX
+	log.Println("Block: ", blockHeight)
+	block, err := b.verus.GetBlockFromHeight(blockHeight)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	for name := range checkForIdentitiesCreation(block) {
-		id, err := b.verus.GetIdentity(name + "@")
-		if err != nil {
-			log.Println("Error:", err)
-			log.Println("For identity:", name)
+	hash, err := b.store.GetBlockHash(block.Height)
+	//if Block is in the db
+	if err == nil {
+		if block.Hash == hash {
+			log.Println("STORE: Block already in databse", block.Height)
+			return block.Hash, nil
+
 		} else {
-			b.Identities[name] = *id
+			log.Println("STORE: Repeated height, wrong hash. Removing all wrong blocks from db", hash, block.Hash)
+			err := b.store.RemoveByHeight(block.Height)
+			if err != nil {
+				return "", err
+			}
+		}
+	}
+
+	var valueTotal int64
+	var blockFee int64
+	var minedCoins int64
+	txCount := int64(len(block.Tx))
+	for _, tx := range block.Tx {
+		coinbaseTx := false
+		var txFee int64
+		var valueTx int64
+		addressesInOut := make(map[string][]bool)
+		for _, vin := range tx.Vin {
+			if vin.Coinbase != "" {
+				coinbaseTx = true
+			}
+			valueTx += vin.ValueSat
+			txFee += vin.ValueSat
+
+			if _, ok := addressesInOut[vin.Address]; !ok {
+				addressesInOut[vin.Address] = make([]bool, 2, 2)
+			}
+			addressesInOut[vin.Address][0] = true
 		}
 
+		for _, vout := range tx.Vout {
+			if coinbaseTx {
+				minedCoins += vout.ValueSat
+			} else {
+				txFee -= vout.ValueSat
+			}
+
+			for _, address := range vout.ScriptPubKey.Addresses {
+				if address == "" {
+					continue
+				}
+				if _, ok := addressesInOut[address]; !ok {
+					addressesInOut[address] = make([]bool, 2, 2)
+				}
+				addressesInOut[address][1] = true
+			}
+
+			if vout.ScriptPubKey.Identityprimary != nil {
+				name := vout.ScriptPubKey.Identityprimary.Name
+				address := vout.ScriptPubKey.Identityprimary.Identityaddress
+				if name != "" {
+					b.store.EnterIdentity(name, address, block.Height, block.Hash)
+				}
+			}
+		}
+
+		for _, vjs := range tx.Vjoinsplit {
+			valueTx -= vjs.VpubNew
+
+		}
+
+		//Fix for transactions with no inputs from
+		if txFee > 0 {
+			blockFee += txFee
+		}
+		if valueTx > 0 {
+			valueTotal += valueTx
+		}
+		for address, v := range addressesInOut {
+			err := b.store.MapTransactionAddress(tx.Txid, address, v[0], v[1], block.Hash, block.Height)
+			if err != nil {
+				return "", err
+			}
+		}
+		err := b.store.EnterTransaction(tx.Txid, len(tx.Vin), len(tx.Vout), txFee, block.Hash, valueTx, block.Height)
+		if err != nil {
+			return "", err
+		}
 	}
-	err = b.store.EnterBlock(block)
-	b.Height++
-	return err
+
+	err = b.store.EnterBlock(block.Height, txCount, valueTotal, minedCoins, blockFee, block.Hash)
+	if err != nil {
+		return "", err
+	}
+	return block.Hash, nil
 }
 
 func (b *BlockchainState) Scan(quit chan bool) {
@@ -86,35 +181,28 @@ func (b *BlockchainState) Scan(quit chan bool) {
 		select {
 		case <-quit:
 			log.Println("Stopping blockchain scan")
-			//b.SaveToDisk()
-			//log.Println("State saved")
 			return
 		default:
 		}
 
 		top, _ := b.verus.GetBlockCount()
-		b.scanBlock()
 
 		for b.Height == top {
 			top, _ = b.verus.GetBlockCount()
 			log.Println("Updating stats")
 			b.CalculateStats()
-			//log.Println("Saving state")
-			//b.SaveToDisk()
-			//log.Println("State saved")
-			select {
-
-			case <-quit:
-				log.Println("Stopping blockchain scan")
-				//log.Println("Saving state")
-				//b.SaveToDisk()
-				//log.Println("State saved")
-				return
-			default:
-			}
-
 			log.Println("Sleeping")
 			time.Sleep(time.Second * 30)
+		}
+		hash, err := b.enterBlock(b.Height)
+		if err != nil {
+			log.Fatal("BLOCKSTATE:", err)
+		}
+		b.Height++
+		b.BlockHash = hash
+		if b.Height%10 == 0 {
+			log.Println("Updating stats")
+			b.CalculateStats()
 		}
 	}
 }
@@ -147,17 +235,13 @@ func (b *BlockchainState) GetLockedIdentities() []identityWithBalance {
 
 func (b *BlockchainState) DeleteBlocks(n int) {
 	top, _ := b.verus.GetBlockCount()
-	b.store.RemoveBlocksByHeight(int64(top - n))
+	b.store.RemoveByHeight(int64(top - n))
 }
 
 func (b *BlockchainState) WriteLastNBlocksToDB(n int) {
 	top, _ := b.verus.GetBlockCount()
 	for i := top - n; i < top; i++ {
-		block, err := b.verus.GetBlockFromHeight(i)
-		if err != nil {
-			log.Fatal(err)
-		}
-		b.store.EnterBlock(block)
+		b.enterBlock(i)
 	}
 	b.Height = top
 }
@@ -174,13 +258,17 @@ func (b *BlockchainState) CalculateStats() (Stats, error) {
 	stats.Week = Summary{}
 	stats.Month = Summary{}
 	stats.Year = Summary{}
-	stats.YearPerHour = make([]Summary, 0)
+	b.StatsPerDay = make([]Summary, 0)
+	b.StatsPerHour = make([]Summary, 0)
 	var hourlyTx, hourlyValue int64
+	var dayTx, dayValue int64
 	for i, block := range blocks {
 		stats.Year.TxCount += block.TxCount
 		stats.Year.Value += block.ValueTotal
 		hourlyTx += block.TxCount
 		hourlyValue += block.ValueTotal
+		dayTx += block.TxCount
+		dayValue += block.ValueTotal
 		//Month
 		if i >= 60*24*(365-30) {
 			stats.Month.TxCount += block.TxCount
@@ -203,9 +291,15 @@ func (b *BlockchainState) CalculateStats() (Stats, error) {
 		}
 
 		if (i+1)%60 == 0 {
-			stats.YearPerHour = append(stats.YearPerHour, Summary{hourlyTx, hourlyValue})
+			b.StatsPerHour = append(b.StatsPerHour, Summary{hourlyTx, hourlyValue})
 			hourlyTx = 0
 			hourlyValue = 0
+		}
+
+		if (i+1)%(60*24) == 0 {
+			b.StatsPerDay = append(b.StatsPerDay, Summary{dayTx, dayValue})
+			dayTx = 0
+			dayValue = 0
 		}
 	}
 	stats.BlockCount = b.Height
--- blockchain/types.go
+++ blockchain/types.go
@@ -1,13 +1,12 @@
 package blockchain
 
 type Stats struct {
-	Hour        Summary   `json:"hour"`
-	Day         Summary   `json:"day"`
-	Week        Summary   `json:"week"`
-	Month       Summary   `json:"month"`
-	Year        Summary   `json:"year"`
-	YearPerHour []Summary `json:"year_per_hour"`
-	BlockCount  int       `json:"block_count"`
+	Hour       Summary `json:"hour"`
+	Day        Summary `json:"day"`
+	Week       Summary `json:"week"`
+	Month      Summary `json:"month"`
+	Year       Summary `json:"year"`
+	BlockCount int     `json:"block_count"`
 }
 
 type Summary struct {
